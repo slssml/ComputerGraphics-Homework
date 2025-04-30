2800fb869b0fbdfeccae0f3d71e97343b4064b30
slssml <eunae86848@gmail.com>
2025-04-30 22:35:23 +0900

    SwipeCar
    SwipeCar 과제

---
+++ Assets/GameDirector.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+
+    void Start()
+    {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+    }
+
+    void Update()
+    {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        if (length >= 0)
+        {
+            this.distance.GetComponent<TextMeshProUGUI>().text = "목표 지점까지: " + length.ToString("F2") + "m";
+        }
+        else
+        {
+            this.distance.GetComponent<TextMeshProUGUI>().text = "게임 오버";
+        }
+    }
+}
